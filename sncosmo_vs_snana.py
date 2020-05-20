6416be548f1603806ce9f1f533cecd0cf667d33a
Mi Dai <nita918@gmail.com>
2020-05-19 23:27:13 -0400

    some updates

--- sncosmo_vs_snana.py
+++ sncosmo_vs_snana.py
@@ -41,8 +41,20 @@ def sncosmo_fitres_to_df(filenames):
         
 def snana_fitres_to_df(filenames):
     surveymap = {53:'CfA3',
+                 54:'CfA4',
+                 56:'Swift',
+                 61:'CfA1',
                  62:'CfA2',
-                 5:'CSP'}
+                 63:'CfA3',
+                 64:'CfA3',
+                 65:'CfA4',
+                 66:'CfA4',
+                 5:'CSP',
+                 150:'Foundation',
+                 1:'SDSS',
+                 15:'PanSTARRS',
+                 50:'Hamuy96',
+                 51:'LOSS'}
     if isinstance(filenames,str):
         filenames = [filenames]
     dflist = []
@@ -87,25 +99,36 @@ def compare_model_sncosmo(data=None,models=[],bands=None,**kwargs):
     fig = sncosmo.plot_lc(data=data,model=models,bands=bands,**kwargs)
     return fig
         
-def get_photometry_single_sn(snname,fmeta=None,flc=None):
-    meta = pd.read_csv(fmeta)
-    lc = pd.read_csv(flc)
+def get_photometry_single_sn(snname,fmeta=None,flc=None,df_meta=None,df_lc=None):
+    meta = df_meta
+    lc = df_lc
+    if meta is None and fmeta is not None:
+        meta = pd.read_csv(fmeta)
+    if lc is None and flc is not None:
+        lc = pd.read_csv(flc)
     meta_sn = meta.loc[meta.Name_upper==snname.upper()]
     lc_sn = lc.loc[lc.Name_upper==snname.upper()]
     return meta_sn,lc_sn
 
-def convert_lc_for_sncosmo(lc,zp=27.5):
+def convert_lc_for_sncosmo(lc,zp=27.5,select_filts=None):
     dflist = []
     for f in lc['Filter'].unique():
-        sn_f = lc.set_index('Filter').loc[f].reset_index()
-        sn_f['flux'] = np.power(10.,-0.4*(sn_f['Mag']-zp))
-        sn_f['flux_err'] = np.absolute(0.921*sn_f['flux']*sn_f['MagErr'])
-        s = sn_f['Survey'].unique()[0]
-        b = f.split('-')[-1]
-        mref,magsys = get_refmag(s,b)
-        sn_f['zp'] = zp-mref
-        sn_f['zpsys'] = magsys
-        dflist.append(sn_f)
+        if isinstance(select_filts,(str,list)) and f.split('-')[1][0] in select_filts:       
+            try:
+                sn_f = lc.set_index('Filter').loc[f].reset_index()
+                sn_f['flux'] = np.power(10.,-0.4*(sn_f['Mag']-zp))
+                sn_f['flux_err'] = np.absolute(0.921*sn_f['flux']*sn_f['MagErr'])
+                s = sn_f['Survey'].unique()[0]
+                b = f.split('-')[-1]
+                mref,magsys = get_refmag(s,b)
+                sn_f['zp'] = zp-mref
+                sn_f['zpsys'] = magsys
+                dflist.append(sn_f)
+            except:
+                print("Error in {} band lc".format(f))
+                continue
+        else:
+            continue
     return Table.from_pandas(pd.concat(dflist,sort=False))
     
 # Function to find common elements in n arrays 
@@ -123,3 +146,19 @@ def commonElements(arr):
         result.intersection_update(currSet) 
   
     return list(result)
+
+def get_mjdrange(group):
+    return pd.Series({'first_fitmjd':group['MJD'].min(),'last_fitmjd':group['MJD'].max()})
+
+def get_snana_fitmjd_range(filenames):
+    if isinstance(filenames,str):
+        filenames = [filenames]
+    dflist = []
+    for filename in filenames:
+        df = pd.read_csv(filename,comment='#',sep='\s+')
+        df_mjd = df.loc[df['DATAFLAG']==1].groupby('CID').apply(get_mjdrange)
+        df_mjd['Survey'] = filename.split('/')[-1].split('_')[0].strip()
+        dflist.append(df_mjd)
+    df_res = pd.concat(dflist,sort=False).reset_index() 
+    return df_res
+    
